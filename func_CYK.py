1c443bd0a778dcb8fafca1ec376c9da796814ca3
Sviatoslav Khmelevskyi <lechstanislaus@gmail.com>
2021-12-22 16:34:32 +0200

    Create function for converting data

--- func_CYK.py
+++ func_CYK.py
@@ -5,6 +5,70 @@ P_r = pd.read_csv("data/rules/P_r.csv").to_numpy()
 P_h = pd.read_csv("data/rules/P_h.csv").to_numpy().reshape((11,11,11))
 P_v = pd.read_csv("data/rules/P_v.csv").to_numpy().reshape((11,11,11))
 
+def DataConverter(S0, S1, SE):
+    """Converts images into a usable form.
+       >>> DataConverter(np.zeros((1,1)), np.ones((1,1)), np.array([[1],[0],[1]]))
+       array([[1],
+              [0],
+              [1]])
+       >>> DataConverter(0, 0, 0)
+       Traceback (most recent call last):
+        ...
+       TypeError: S0, S1 and SE must be numpy arrays
+       >>> DataConverter(np.zeros((1,1)), np.ones((2,1)), np.array([[1],[0],[1]]))
+       Traceback (most recent call last):
+        ...
+       ValueError: wrong shape of S0 and S1
+       >>> DataConverter(np.zeros((1,2)), np.ones((1,2)), np.array([[1],[0],[1]]))
+       Traceback (most recent call last):
+        ...
+       ValueError: wrong shape of SE
+       >>> DataConverter(np.zeros((1,1)) + 2, np.ones((1,1)), np.array([[1],[0],[1]]))
+       Traceback (most recent call last):
+        ...
+       ValueError: elements of S0 and S1 must be equal to 0 or 1
+       >>> DataConverter(np.zeros((1,1)), np.ones((1,1)), np.array([[2],[0],[2]]))
+       Traceback (most recent call last):
+        ...
+       ValueError: elements of SE must be equal to 0 or 1
+       >>> DataConverter(np.zeros((1,2)), np.ones((1,2)), np.array([[1, 0],[0, 0],[1, 1]]))
+       Traceback (most recent call last):
+        ...
+       ValueError: SE does not correspond to S0 and S1
+    """
+    if type( S0 ) != np.ndarray or type( S1 ) != np.ndarray or type( SE ) != np.ndarray:
+        raise TypeError( "S0, S1 and SE must be numpy arrays" )
+    if S0.shape[0] != S1.shape[0] or S0.shape[1] != S1.shape[1]:
+        raise ValueError( "wrong shape of S0 and S1" )
+    if SE.shape[0] != 3 * S0.shape[0] or SE.shape[1] % S0.shape[1] != 0:
+        raise ValueError( "wrong shape of SE" )
+    if np.min(np.isin(S0, np.array([0, 1]))) == 0 or np.min(np.isin(S1, np.array([0, 1]))) == 0:
+        raise ValueError( "elements of S0 and S1 must be equal to 0 or 1" )
+    if np.min(np.isin(SE, np.array([0, 1]))) == 0:
+        raise ValueError( "elements of SE must be equal to 0 or 1" )
+    h, w = S0.shape[0], S0.shape[1]
+    c = SE.shape[1] // w
+    X = np.zeros((3, c), dtype = int)
+
+    if len(S0.shape) == 3:
+        S0 = S0[:,:,0]
+    if len(S1.shape) == 3:
+        S1 = S1[:,:,0]
+    if len(SE.shape) == 3:
+        SE = SE[:,:,0]
+
+    for i in range(3):
+        for j in range(c):
+            if np.all(SE[h * i:h * (i + 1),w * j:w * (j + 1)] == S0):
+                X[i,j] = 0
+            elif np.all(SE[h * i:h * (i + 1),w * j:w * (j + 1)] == S1):
+                X[i,j] = 1
+            else:
+                X[i,j] = 2
+    if np.min(np.isin(X, np.array([0, 1]))) == 0:
+        raise ValueError( "SE does not correspond to S0 and S1" )
+    return X
+
 def CYK(X, D = {}):
     """Implements modification of CYK algorithm.
        >>> CYK(np.array([[0],[0],[0]]))[0,0]
--- main_CYK.py
+++ main_CYK.py
@@ -1,8 +1,9 @@
 import numpy as np
+import imageio as im
 import func_CYK as ck
 
-A = np.array( [[1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1],
-               [1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0],
-               [0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1]] )
+Z = (np.array(im.imread("data/standards/zero.png")) / 255).astype(int)
+F = (np.array(im.imread("data/standards/one.png")) / 255).astype(int)
+E = (np.array(im.imread("data/examples/example_3.png")) / 255).astype(int)
 
-ck.AnswerFilter( ck.CYK( A ) )
+ck.AnswerFilter( ck.CYK(ck.DataConverter(Z, F, E) ) )
